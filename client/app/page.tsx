d92d819a7ade7abbcaa4100da889fda7835d184a
neha-patankar <nehapatankar122@gmail.com>
2026-02-10 12:53:28 +0530

    all done in frontend

---
+++ client/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import ImportHistory from "./components/ImportHistory"
+
+export default function Home() {
+  return (
+    <div>
+   
+      <ImportHistory/>
+    </div>
+  );
+}
